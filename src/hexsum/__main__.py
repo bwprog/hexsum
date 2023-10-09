71325609eca26cbac583a321f9295a39d40003e1
Brandon Wells <b.w.prog@outlook.com>
2023-10-08 22:21:40 -0400

    -Add typing Any for hash type checking -Add perf counters and ending time completion -Refactored Output from Panel to Table for simple, cleaner code and look

--- src/hexsum/__main__.py
+++ src/hexsum/__main__.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
-"""hexsum: A cli tool to use python hashlib wrapped around OpenSSL to generate hex sums."""
+"""hexsum: A cli tool to use python hashlib wrapped around OpenSSL to generate hex sums.
+
+TODO: Guard against memory consumption by cycling through file read and hex update.
+TODO: Add rich progress for file read and hex updates.
+TODO: add file size to the output.
+"""
 
 __author__ = 'Brandon Wells'
 __email__ = 'b.w.prog@outlook.com'
@@ -7,19 +12,24 @@ __copyright__ = '© 2023 Brandon Wells'
 __license__ = 'GPL3+'
 __status__ = 'Development'
 __update__ = '2023.10.08'
-__version__ = '0.9.2'
+__version__ = '0.9.4'
 
 
 import hashlib
 from collections.abc import Callable
 from pathlib import Path
-from typing import Annotated, Optional
+from time import perf_counter
+from typing import Annotated, Any, Optional
 
 import typer
 from rich.console import Console
 from rich.panel import Panel
 from rich.table import Table
 from rich.traceback import install
+
+# constant for tracking program duration
+PROG_TIME_START: float = perf_counter()
+
 
 # rich enhancements: console output | install provides traceback | rich print (rp)
 console = Console()
@@ -32,7 +42,9 @@ hash_list: list[str] = sorted(hashlib.algorithms_guaranteed)
 
 
 # ~~~ #     - typer callback function -
-def callback_version(version: bool) -> None:
+def callback_version(
+        version: bool,
+) -> None:
     """Print version and exit.
 
     Parameters
@@ -51,7 +63,9 @@ def callback_version(version: bool) -> None:
 
 
 # ~~~ #     - typer callback function -
-def callback_available(available: bool) -> None:
+def callback_available(
+        available: bool,
+) -> None:
     """Derive and print all available hash types.
 
     Parameters
@@ -70,7 +84,7 @@ def callback_available(available: bool) -> None:
         hash_table.add_column(
             header='Hash',
             justify='right',
-            style='#cb4b16',
+            style='blue',
             no_wrap=True,
         )
         hash_table.add_column(
@@ -93,12 +107,13 @@ def callback_available(available: bool) -> None:
         )
         for i in hash_list:
             match i:
-                case 'shake':
+                # share has a length option that must be segregated out
+                case i if 'shake' in i:
                     hash_table.add_row(
                         i,
                         str(getattr(hashlib, i)().block_size),
-                        '32 (or -l)',
-                        '64 (or 2 * -l)',
+                        '32 (or [-l (int)])',
+                        '64 (or 2 * [-l (int)])',
                     )
                 case _:
                     hash_table.add_row(
@@ -113,7 +128,9 @@ def callback_available(available: bool) -> None:
 
 
 # ~~~ #     - typer callback function -
-def callback_length(length: int) -> int:
+def callback_length(
+        length: int,
+) -> int:
     """Validate the CLI length Option.
 
     Parameters
@@ -147,7 +164,9 @@ def callback_length(length: int) -> int:
 
 
 # ~~~ #     - typer callback function -
-def callback_hash(hash_type: str) -> list[str]:
+def callback_hash(
+        hash_type: str,
+) -> list[str]:
     """Validate the hash requested and return a list with it.
 
     Parameters
@@ -183,7 +202,9 @@ def callback_hash(hash_type: str) -> list[str]:
 
 
 # ~~~ #     - typer callback function -
-def callback_compare(compare: str) -> str | None:
+def callback_compare(
+        compare: str,
+) -> str | None:
     """Validate the compare CLI option is a valid hexadecimal number.
 
     Parameters
@@ -222,15 +243,21 @@ def callback_compare(compare: str) -> str | None:
 
 
 # ~~~ #     - the hashlib function to derive hex values -
-def render_hex(hex_dict: dict, hash_value) -> str:
+def render_hex(
+        hash_type: str,
+        file: Path,
+        length: int,
+) -> str:
     """Read the file, hash it, and return a hex value.
 
     Parameters
     ----------
-    hex_dict : dict
-        main dictionary containing all the variables
-    hash_value : bool
+    hash_type : str
         the specific hash type to run for this iteration
+    file : Path
+        the file to hash
+    length : int | None
+        the length as an int to use if the hash type is shake
 
     Returns
     -------
@@ -243,16 +270,17 @@ def render_hex(hex_dict: dict, hash_value) -> str:
         if the file cannot be read (OSError)
     """
     try:
-        with Path.open(hex_dict['file'], mode='rb') as f:
-            hashed_value = hex_dict['hex_function'][hash_value](f.read())
-            if 'shake' in hash_value:
-                return hashed_value.hexdigest(hex_dict['length'])
-            else:
-                return hashed_value.hexdigest()
+        with Path.open(file, mode='rb') as f:
+            hashed_value: Any = getattr(hashlib, hash_type)(f.read())
+            match hash_type:
+                case hash_type if 'shake' in hash_type:
+                    return hashed_value.hexdigest(length)                                       # type: ignore PGH003
+                case _:
+                    return hashed_value.hexdigest()
 
     except OSError as e:
         rp(Panel(
-            f'cannot read file: {hex_dict["file"]}\n{e}.',
+            f'cannot read file: {file}\n{e}.',
             title='[bold]Error[/]',
             title_align='left',
             border_style='red',
@@ -263,13 +291,27 @@ def render_hex(hex_dict: dict, hash_value) -> str:
 
 
 # ~~~ #     - rich print console output -
-def output_final(hex_dict: dict) -> None:
+def output_final(
+        compare: str | None,
+        file: Path,
+        hash_type_list: list[str],
+        hex_values: dict[str, str],
+        length: int,
+) -> None:
     """Rich print the panel of hex value(s).
 
     Parameters
     ----------
-    hex_dict : dict
-        main dictionary containing all the variables
+    compare : str | None
+        the optional cli --compare/-c hex value to compare to
+    file : Path
+        the file name that was hashed in Path object format
+    hash_type_list : list[str]
+        list of hashes to derive values for
+    hex_values : dict[str, str]
+        the derived hex values with k = hash name and v = hex value
+    length : int
+        length to use for shake hashes
 
     Raises
     ------
@@ -278,52 +320,61 @@ def output_final(hex_dict: dict) -> None:
     """
     # Initial output of the program name and version
     rp(f'\n{ver}\n', highlight=False)
+
     # uncomment the next line to view all variables for troubleshooting
     # console.log('output_final function', log_locals=True)
 
-    # rich panel attributes
-    hex_panel_content: str = ''
-    hex_panel_title: str = ''
-    hex_panel_border_style: str = ''
+    # build hex table for final output
+    hex_table = Table(title=f'Hex Value(s) for [green]{file}[/]')
+    hex_table.add_column(
+        header='Hash',
+        justify='center',
+        style='blue',
+        no_wrap=True,
+    )
+    hex_table.add_column(
+        header='Hex Value',
+        justify='center',
+        style='bold white',
+        no_wrap=False,
+        overflow='fold',
+    )
 
-    # 3 print options: compare | all | regular
-    if hex_dict['compare']:
-        h = hex_dict['hash_type_list'][0]
-        # h_d (hash display) used to add the length to the shake hash display
-        h_d = f'{h}({hex_dict["length"]})' if 'shake' in h else h
-        hex_panel_content = f'Generated: {hex_dict["hex_value"][h]}\n'
-        hex_panel_content += f'Compared:  {hex_dict["compare"]}'
-        # validate hex values match and print accordingly
-        if hex_dict['compare'] == hex_dict['hex_value'][h]:
-            hex_panel_title = f'[bold]{h_d}[/] Hex Value for {hex_dict["file"]}'
-            hex_panel_title += ' [bold]MATCH![/]'
-            hex_panel_border_style = 'green'
+    if compare:
+        hex_table.add_column(
+            header='Origin',
+            justify='center',
+            style='blue',
+            no_wrap=True,
+        )
+        # short name for hash type
+        h: str = hash_type_list[0]
+        # short printable hash name including the shake length if applicable
+        d: str = f'{h}({length})' if 'shake' in h else h
+        hex_table.add_row(
+            d,
+            hex_values[h],
+            'Generated',
+        )
+        hex_table.add_row(
+            d,
+            compare,
+            'Provided',
+        )
+        if compare == hex_values[h]:
+            hex_table.title = f'[bold]{d}[green] Hex Value for {file} MATCHES![/]'
         else:
-            hex_panel_title = f'[bold]{h_d}[/] Hex Value for {hex_dict["file"]}'
-            hex_panel_title += ' [bold]DO NOT MATCH!!![/]'
-            hex_panel_border_style = 'red'
-    elif len(hex_dict['hash_type_list']) > 1:
-        hex_panel_title = f'Hex Values for {hex_dict["file"]}'
-        hex_panel_border_style = 'green'
-        hex_panel_content = '[bold blue]hash[/]\t\t[bold green]Value[/]\n'
-        for i, h in enumerate(hex_dict['hash_type_list']):
-            # h_d (hash display) used to add the length to the shake hash display
-            h_d = f'{h}({hex_dict["length"]})' if 'shake' in h else h
-            hex_panel_content += f'[bold]{h_d:<16s}[/]{hex_dict["hex_value"][h]}'
-            hex_panel_content += '\n' if i < (len(hex_dict['hash_type_list']) - 1) else ''
+            hex_table.title = f'[bold]{d}[red] Hex Value for {file} DOES NOT MATCH!!![/]'
     else:
-        h = hex_dict['hash_type_list'][0]
-        # h_d (hash display) used to add the length to the shake hash display
-        h_d = f'{h}({hex_dict["length"]})' if 'shake' in h else h
-        hex_panel_title = f'[bold]{h_d}[/] Hex Value for {hex_dict["file"]}'
-        hex_panel_border_style = 'green'
-        hex_panel_content = hex_dict['hex_value'][h]
+        for k, v in hex_values.items():
+            d: str = f'{k}({length})' if 'shake' in k else k
+            hex_table.add_row(
+                d,
+                v,
+            )
 
     # rich print and rich panel to display one of the 3 output types
-    rp(Panel(hex_panel_content, title=hex_panel_title, title_align='left',
-             border_style=hex_panel_border_style, highlight=True))
-
-    raise typer.Exit
+    rp(hex_table)
 
 
 # ~~~ #     - CLI variables are here in main for typer -
@@ -383,28 +434,27 @@ def main(
         )
         raise typer.Exit
 
-    # create hashing and hex dictionary
-    hex_dict = {
-        'file': file,
-        'file_size': Path(file).stat().st_size,
-        'hash_type_list': hash_type_list,
-        'length': length,
-        'compare': compare,
-        'hex_function': {h: getattr(hashlib, h) for h in hash_type_list},
-        'hex_value': {},
-    }
-
     # iterate through requested hash or hashes deriving hex values
-    for hash_type in hex_dict['hash_type_list']:
-        hex_dict['hex_value'][hash_type] = render_hex(hex_dict=hex_dict,
-                                                      hash_value=hash_type)
+    hex_values: dict[str, str] = {h: render_hex(hash_type=h, file=file, length=length) for h in hash_type_list}
 
     # call the fancy output function
-    output_final(hex_dict=hex_dict)
+    output_final(
+        compare=compare,
+        file=file,
+        hash_type_list=hash_type_list,                                                          # type: ignore PGH003
+        hex_values=hex_values,
+        length=length,
+    )
+
+    # exit the app
+    prog_time_total: float = perf_counter() - PROG_TIME_START
+    rp(Panel(f':glowing_star: Complete :glowing_star: ([green]{prog_time_total:.4f}[/]s)',
+             border_style='green'), highlight=False)
+    raise typer.Exit
 
 
 # ~~~ #
 if __name__ == '__main__':
 
     # use typer to build cli arguments off main variables
-    typer.run(main)
+    typer.run(function=main)
